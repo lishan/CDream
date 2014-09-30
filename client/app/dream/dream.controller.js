16723dd804953755a262514d4a28008d9c56ea0e
yulishan3@gmail.com <tfa919189>
2014-09-30 13:49:21 +0800

    first dream part

--- client/app/dream/dream.controller.js
+++ client/app/dream/dream.controller.js
@@ -4,10 +4,19 @@ angular.module('cdreamApp')
   .controller('DreamCtrl', function ($scope, $http, loginService, utilService, $routeParams) {
     loginService.getCookieData($scope);
     $scope.softVersion = loginService.getSoftwareVersion();
+
+    $http.get("/api/dreams/" + $routeParams.dreamId).success(function(dream){
+      $scope.dream = dream;
+    });
     $scope.choose = function (dream) {
       if (dream !== null && dream !== undefined) {
-        return $routeParams.dreamId === dream.id;
+        return $routeParams.dreamId === dream._id;
       }
       return false;
+    }
+
+    $scope.changeToMarkDown = function(str){
+        console.log(str);
+        return toMarkdown(str);
     }
   });
