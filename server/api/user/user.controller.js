6680b985223c2e42e01f794260991ad109bcdad5
Yulishan <yulishan3@gmail.com>
2014-10-16 19:34:12 +0800

    update

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -5,125 +5,184 @@ var User = require('./user.model');
 var Dream = require('../dream/dream.model');
 
 // Get list of users
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   User.find(function (err, users) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, users);
   });
 };
 
-exports.find = function(req ,res){
-  User.findOne({'email' : req.params.email,'pass' : req.params.pass},function(err, user){
-    if(err){return handleError(res,err)}
+exports.find = function (req, res) {
+  User.findOne({'email': req.params.email, 'pass': req.params.pass}, function (err, user) {
+    if (err) {
+      return handleError(res, err)
+    }
     return res.json(user);
   });
 };
 
-exports.findEmail = function(req ,res){
-  User.findOne({'email' : req.params.email},function(err, user){
-    return !err;
+exports.findEmail = function (req, res) {
+  User.findOne({'email': req.params.email}).populate('dreams tags').exec(function (err, user) {
+    if (err) {
+      return handleError(res, err);
+    }
+    return res.json(user);
   });
 };
 
-exports.findUser = function(req, res){
-  User.findOne({'email' : req.params.email}).populate('dreams tags').exec(function(err, user){
-    Dream.populate(user.dreams,{path : '_user tasks'},function(err,dreams){
-       if(err){return handleError(res,err)}
-       user.dreams = dreams;
-       return res.json(user);
+exports.findUser = function (req, res) {
+  User.findOne({'email': req.params.email}).populate('dreams tags').exec(function (err, user) {
+    Dream.populate(user.dreams, {path: '_user tasks'}, function (err, dreams) {
+      if (err) {
+        return handleError(res, err)
+      }
+      user.dreams = dreams;
+      return res.json(user);
     });
-    if(err){return handleError(res,err)}
+    if (err) {
+      return handleError(res, err)
+    }
   });
 };
 
-exports.addDream = function(req, res){
-  if(req.body._id) { delete req.body._id; }
+exports.addDream = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   User.findById(req.params.id, function (err, users) {
-    if (err) { return handleError(res, err); }
-    if(!users) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!users) {
+      return res.send(404);
+    }
     users.dreams.push(req.body.dream);
     users.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, users);
     });
   });
 };
 
-exports.removeDream = function(req, res){
+exports.removeDream = function (req, res) {
   User.findById(req.params.id, function (err, users) {
-    if (err) { return handleError(res, err); }
-    if(!users) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!users) {
+      return res.send(404);
+    }
     users.dreams.pull(req.body.dream);
     users.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, users);
     });
   });
 };
 
-exports.addTag = function(req, res){
-  if(req.body._id) { delete req.body._id; }
+exports.addTag = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   User.findById(req.params.id, function (err, users) {
-    if (err) { return handleError(res, err); }
-    if(!users) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!users) {
+      return res.send(404);
+    }
     users.tags.push(req.body.tag);
     users.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, users);
     });
   });
 };
 
-exports.removeTag = function(req, res){
+exports.removeTag = function (req, res) {
   User.findById(req.params.id, function (err, users) {
-    if (err) { return handleError(res, err); }
-    if(!users) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!users) {
+      return res.send(404);
+    }
     users.tags.pull(req.body.tag);
     users.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, users);
     });
   });
 };
 
 // Get a single user
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   User.findById(req.params.id, function (err, user) {
-    if(err) { return handleError(res, err); }
-    if(!user) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!user) {
+      return res.send(404);
+    }
     return res.json(user);
   });
 };
 
 // Creates a new user in the DB.
-exports.create = function(req, res) {
-  User.create(req.body, function(err, user) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  User.create(req.body, function (err, user) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, user);
   });
 };
 
 // Updates an existing user in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   User.findById(req.params.id, function (err, user) {
-    if (err) { return handleError(res, err); }
-    if(!user) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!user) {
+      return res.send(404);
+    }
     var updated = _.merge(user, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, user);
     });
   });
 };
 
 // Deletes a user from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   User.findById(req.params.id, function (err, user) {
-    if(err) { return handleError(res, err); }
-    if(!user) { return res.send(404); }
-    user.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!user) {
+      return res.send(404);
+    }
+    user.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
