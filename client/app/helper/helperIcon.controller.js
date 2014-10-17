8fcfbba724b0dc04b9073c1b7a1784848cd5b151
yulishan3@gmail.com <tfa919189>
2014-10-17 12:29:48 +0800

    admin

--- client/app/helper/helperIcon.controller.js
+++ client/app/helper/helperIcon.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('cdreamApp')
   .controller('HelperIconCtrl', function ($scope, loginService) {
-    loginService.getCookieData($scope);
+//    loginService.getCookieData($scope);
     $scope.softVersion = loginService.getSoftwareVersion();
 
 
