6d526b9c3834823c5bfe28260e01a4ada440ef09
yulishan3@gmail.com <tfa919189>
2014-10-24 14:11:25 +0800

    remove useless test

--- server/routes.js
+++ server/routes.js
@@ -19,6 +19,8 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
+  app.route('/*').get(errors[404]);
+
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
